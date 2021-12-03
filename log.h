5912d982c3d2cb56819ff6fef08c3ec82ec808a1
JAY <jlei10@jhu.edu>
2021-12-02 22:40:07 -0500

    Update log.h

--- log.h
+++ log.h
@@ -23,22 +23,22 @@ public:
     void add_to_log(shared_ptr<Update> update) {
         assert(update != nullptr);
         cout << "   Adding update to the log" << endl;
-        server_2_update_id[update->server_id].insert(update->mail_id); // this update belongs to this server
-        assert(id_2_update.count(update->mail_id) == 0); // there is no such update before
-        id_2_update[update->mail_id] = update; // preserve the update in the memory
+        server_2_update_id[update->server_id].insert(update->timestamp); // this update belongs to this server
+        assert(id_2_update.count(update->timestamp) == 0); // there is no such update before
+        id_2_update[update->timestamp] = update; // preserve the update in the memory
         //TODO: add to file
     }
 
     // delete update to an update
-    void delete_update(const int server_id, const string & mail_id) {
+    void delete_update(const int server_id, const int64_t & timestamp) {
         assert(server_2_update_id.count(server_id) == 1); // must have it before delete it
-        server_2_update_id[server_id].erase(mail_id); // do not preserve this update for this server
-        id_2_update.erase(mail_id); // do not preserve this update in memory
+        server_2_update_id[server_id].erase(timestamp); // do not preserve this update for this server
+        id_2_update.erase(timestamp); // do not preserve this update in memory
         // TODO: delete from file
     }
 private:
-    unordered_map<int, set<string>> server_2_update_id; // mail id
-    unordered_map<string, shared_ptr<Update>> id_2_update;
+    unordered_map<int, set<int64_t>> server_2_update_id; // mail id
+    unordered_map<int64_t, shared_ptr<Update>> id_2_update;
 };
 
 #endif //MAIL_SYSTEM_STATE_H
