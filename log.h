dc7736877506ed60c486c26a40830106b2f81715
JAY <jlei10@jhu.edu>
2021-12-02 22:22:35 -0500

    Update log.h

--- log.h
+++ log.h
@@ -5,6 +5,14 @@
 #ifndef MAIL_SYSTEM_STATE_H
 #define MAIL_SYSTEM_STATE_H
 #include "common_include.h"
+#include <set>
+
+struct MyComp {
+    bool operator() ( shared_ptr<Update>& a,  shared_ptr<Update>& b) {
+        return a->timestamp < b->timestamp;
+    }
+} myComp;
+
 class Log {
 public:
     Log():server_2_updates(){
@@ -13,21 +21,26 @@ public:
 
     // add an update to the log file
     void add_to_log(shared_ptr<Update> update) {
+        assert(update != nullptr);
         cout << "   Adding update to the log" << endl;
-        server_2_updates[update->server_id].push_back(update);
+        if(server_2_updates.count(update->server_id) == 0)
+            server_2_updates.insert(make_pair(update->server_id, set<shared_ptr<Update>, MyComp>(myComp)));
+        server_2_updates[update->server_id].insert(update);
         //TODO: add to file
     }
 
     // delete update to an update
     void delete_update(const int server_id, int64_t timestamp) {
         vector<shared_ptr<Update>> new_vec;
-        while(server_2_updates[server_id].back()->timestamp >= timestamp){
-            server_2_updates[server_id].pop_back();
+        while( (*server_2_updates[server_id].rbegin())->timestamp >= timestamp){
+            auto it = server_2_updates[server_id].end();
+            --it;
+            server_2_updates[server_id].erase(it);
         }
         // TODO: delete from file
     }
 private:
-    unordered_map<int, vector<shared_ptr<Update>>> server_2_updates;
+    unordered_map<int, set<shared_ptr<Update>, MyComp>> server_2_updates;
 };
 
 #endif //MAIL_SYSTEM_STATE_H
